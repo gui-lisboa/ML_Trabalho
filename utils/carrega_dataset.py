8f38a66f535a418937ef3f478b006beb72f92222
Guilherme <g.lisboa.oliveira@outlook.com>
2022-05-06 10:22:27 -0300

    remove coluna Row# no carregamento do dataset

--- utils/carrega_dataset.py
+++ utils/carrega_dataset.py
@@ -4,5 +4,6 @@ CAMINHO_CSV = './dataset/WildBlueberryPollinationSimulationData.csv'
 
 def carrega_dataset():
     dataset = pd.read_csv(CAMINHO_CSV)
+    dataset.drop(['Row#'], axis=1, inplace=True)
     dataset = dataset.convert_dtypes()
     return dataset
